63c0056acdfac08140a12c0263e9998b83d409e7
Hector Perez Baranda <ticolotico@gmail.com>
2018-08-22 01:26:14 -0300

    implementing multiples parameters in api call

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -6,24 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api_relatorio_transacoes.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/data")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET api/data/5
+        [HttpGet("{a}")]
+        public ActionResult<string> Get(string a)
+        {
+            return "value > "+a;
+        }
+
+        // GET api/data/?id=5
+        [HttpGet]
+        public ActionResult<string> Get(int id=3)
+        {
+            return "value: "+id;
+        }
+
+        
+    }
+
+    [Route("api/otro")]
+    
+    public class MioController : ControllerBase
+    {
+        // GET api/otro
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/value/5
+        // Get api/otro/3
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id=3)
+        public ActionResult<string> Get(int id)
         {
-            return "value: "+id;
+            return  "value2: "+id ;
         }
-
         
     }
 }
